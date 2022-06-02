f791592cfd7a437054fd0b52f4fb441cf57ec835
XRichardroidX <103499139+XRichardroidX@users.noreply.github.com>
2022-06-02 22:02:54 +0100

    I added Nice 'cause I love Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// Nice...
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -25,4 +26,3 @@ void main() => runApp(ChangeNotifierProvider(
         },
       ),
 ));
-
